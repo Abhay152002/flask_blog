3da0981ab02c075b2cac1464e1e92be4531edac0
Abhay152002 <achaturvedi082@gmail.com>
2024-04-27 08:49:43 +0530

    first commit

---
+++ flaskblog/conifg.py
@@ -0,0 +1,9 @@
+class Config:
+    SECRET_KEY= '7eda7a86ef7dbe5b38871d233c77dc4e'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+
+    MAIL_SERVER='smtp.gmail.com'
+    MAIL_PORT=465
+    MAIL_USE_SSL=True
+    MAIL_USERNAME='achaturvedi082@gmail.com'
+    MAIL_PASSWORD='xyhy bsbv fczx hhjs'
